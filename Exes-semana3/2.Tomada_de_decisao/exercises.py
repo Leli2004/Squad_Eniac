1d48e768672c6b7054183a406d56e3635ddead9c
Elisa-Libanio <67328950+Elisa-Libanio@users.noreply.github.com>
2024-01-20 11:51:22 -0300

    Update exercises.py

--- Exes-semana3/2.Tomada_de_decisao/exercises.py
+++ Exes-semana3/2.Tomada_de_decisao/exercises.py
@@ -35,29 +35,3 @@
 # else:
 #     print("Entrada inválida ")
 
-# #atividade5:
-
-# # equilátero:todos os lados com o mesmo valor 
-# # isósceles:dois lados com o mesmo valor 
-# # escaleno:todos os lados com medidas distintas
-
-# lado_a = (int(input("Insira a medida do primeiro lado:")))
-# lado_b = (int(input("Insira a medida do segundo lado:")))
-# lado_c= (int(input("Insira a medida do segundo lado:")))
-
-# if lado_a == lado_b == lado_c:
-#     print("É equilatero")
-# elif (lado_a == lado_b) or (lado_a == lado_c) or (lado_b == lado_c):
-#     print("É isósceles")
-# elif (lado_a != lado_b) and (lado_b != lado_c) and (lado_c != lado_a):
-#     print("É escaleno ")
-# #ps: verificar como faz pra saber se nao é triangulo e adicionar um else ao final.
-    
-#atividade6
-login = (input(" Insira seu nome de usuario:"))
-
-senha = (input("Insira sua senha:")) 
-
-if login == "admin" and senha == "admin123":
-    print("acesso liberado")
-else: print("login e senha inválidos")
