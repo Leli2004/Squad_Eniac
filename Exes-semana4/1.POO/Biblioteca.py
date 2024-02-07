15af18ea01a89a40d744edd81116d2558a6ec23c
Josiane Silva <josiannesilwa@gmail.com>
2024-02-07 00:04:32 -0300

    Atualizacao dos metodos

--- Exes-semana4/1.POO/Biblioteca.py
+++ Exes-semana4/1.POO/Biblioteca.py
@@ -10,16 +10,16 @@ class Livro:
         self.quantidade_disponivel = 10
 
     def adicionarLivro(self, id, autor, titulo,editora, genero, quantidade_disponivel):
-        self.id = id
+        print("adicionando um livro")
     
-    def excluirLivro(self, id, autor, genero):
-        self.id = id
+    def excluirLivro(self, id, titulo):
+        print("Livro excluido")
 
     def editarLivroAdicionado(self, id):
-        self.id = id
+        print("Livro atualizado")
     
-    def consultarLivro(self, id, autor, genero):
-        self.id = id
+    def consultarLivro(self, autor, genero):
+        print("Livros disponiveis")
 
 class Usuario:
     def __init__(self, id, nome, telefone, nacionalidade):
@@ -28,11 +28,11 @@ class Usuario:
         self.telefone = telefone
         self.nacionalidade = nacionalidade
     
-    def emprestarLivro(self, id, ):
-        self.id = id
+    def emprestarLivro(self, nome):
+        print(f'{nome} emprestou um livro')
     
-    def devolverLivro(self, id, ):
-        self.id = id
+    def devolverLivro(self, nome ):
+        print(f'{nome} devolveu um livro')
 
 
 class Emprestimo:
@@ -42,11 +42,22 @@ class Emprestimo:
         self.data_devolucao = data_devolucao
         self.estado_exemplar = disponivel
 
-    def retiradaDeLivro(self, id):
-        self.id = id
+    def retiradaDeLivro(self, id, data_emprestimo,disponivel):
+        print(f'Livro retirado na data {data_emprestimo}, false')
     
-    def devolucaoDeLivro(self, id):
-        self.id = id
+    def devolucaoDeLivro(self, id, data_devolucao):
+        print(f'Livro devolvido na data {data_devolucao}, false')
 
-    def consultarEmprestimo(self, id):
-        self.id = id
+    def consultarEmprestimo(self, id, nome, data_emprestimo):
+        print(f'Registro dos livros devolvidos e emprestado')
+
+l1 = Livro(1, "manoel bandeira", "Aurora", "Atlas", "literatura", 12)
+l1.adicionarLivro(1, "manoel bandeira", "Aurora", "Atlas", "literatura", 12)
+l1.excluirLivro(1, "aurora")
+
+u1 = Usuario(1, "Alexandre Dimas", 129923457, "brasileiro")
+u1.emprestarLivro("Alexandre Dimas")
+
+e1 = Emprestimo(1, 12012024,12022024, "emprestado")
+e1.retiradaDeLivro(1, 12012024, "emprestado")
+e1.emprestimo()
